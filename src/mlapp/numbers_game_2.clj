06a3fa3461d1cb036a8b3584821fcb35e66915f1
Edmund Jackson <edmundsjackson@gmail.com>
2013-01-08 21:27:24 +0000

    Nice setup for hypothesis testing.

--- src/mlapp/numbers_game_2.clj
+++ src/mlapp/numbers_game_2.clj
@@ -7,64 +7,73 @@
    [com.stuartsierra.flow :as flow]))
 
 ;; Helpers
-(defn s-cat [s1 s2]
-  (symbol (str s1 (name s2))))
-
-(defn prior-sym [id]
-  (s-cat "prior-" id))
+(defn k-cat [s1 s2]
+  (keyword (str s1 (name s2))))
 
 (defn prior-key [id]
-  (keyword (prior-sym id)))
-
-(defn likelihood-fn-sym [id]
-  (s-cat "likelihood-fn-" id))
+  (k-cat "prior-" id))
 
 (defn likelihood-fn-key [id]
-  (keyword (likelihood-fn-sym id)))
-
-(defn likelihood-sym [id]
-  (s-cat "likelihood-" id))
+  (k-cat "likelihood-fn-" id))
 
 (defn likelihood-key [id]
-  (keyword (likelihood-sym id)))
-
-(defn unnorm-posterior-sym [id]
-  (s-cat "unnorm-posterior-" id))
+  (k-cat "likelihood-" id))
 
 (defn unnorm-posterior-key [id]
-  (keyword (unnorm-posterior-sym id)))
+  (k-cat "unnorm-posterior-" id))
 
-(defn posterior-sym [id]
-  (s-cat "posterior-" id))
-
-(def posterior-key (comp keyword posterior-sym))
+(defn posterior-key [id]
+  (k-cat "posterior-" id))
 
 (defn add-hypothesis [flow {:keys [id likelihood-fn prior] :as hypothesis}]
-  (-> flow
-      (assoc (prior-key id)            (flow/flow-fn [] prior))
-      (assoc (likelihood-fn-key id)    (flow/flow-fn [] likelihood-fn))
-      (assoc (likelihood-key id)       (flow/with-inputs [(likelihood-fn-key id) :data]
-                                         (eval`(fn [{:keys [~(likelihood-fn-sym id) ~'data]}]
-                                                 (~(likelihood-fn-sym id) ~'data)))))
-      (assoc (unnorm-posterior-key id) (flow/with-inputs [(likelihood-key id) (prior-key id)]
-                                         (eval`(fn [{:keys [~(likelihood-sym id) ~(prior-sym id)]}]
-                                                 (* ~(likelihood-sym id) ~(prior-sym id))))))
-      (assoc (posterior-sym id)        (flow/with-inputs [(unnorm-posterior-key id) :norm]
-                                         (eval`(fn [{:keys [~(unnorm-posterior-sym id) ~'norm]}]
-                                                 (/ ~(unnorm-posterior-sym id) ~'norm)))))))
-
-(defn norm-fn [ids]
-  `(flow/with-inputs ~(vec (map unnorm-posterior-key ids))
-     (fn [{:keys ~(vec (map unnorm-posterior-sym ids))}]
-       (+ ~@(map unnorm-posterior-sym ids)))))
+  (let [prior-k              (prior-key id)
+        likelihood-fn-k      (likelihood-fn-key id)
+        likelihood-k         (likelihood-key id)
+        unnorm-posterior-k   (unnorm-posterior-key id)
+        posterior-k          (posterior-key id)]
+   (-> flow
+       (assoc prior-k                   (flow/flow-fn [] prior))
+       (assoc likelihood-fn-k           (flow/flow-fn [] likelihood-fn))
+       (assoc likelihood-k              (flow/with-inputs [likelihood-fn-k :data]
+                                          (fn [{data :data :as r}]
+                                            ((likelihood-fn-k r) data))))
+       (assoc unnorm-posterior-k        (flow/with-inputs [likelihood-k prior-k]
+                                          (fn [r]
+                                            (* (likelihood-k r) ((prior-key id) r)))))
+       (assoc (posterior-key id)        (flow/with-inputs [unnorm-posterior-k :norm]
+                                          (fn [r]
+                                            (/ (unnorm-posterior-k r) (:norm r))))))))
 
 (defn normalise-flow [flow hypotheses]
-  (assoc flow :norm (eval (norm-fn (map :id hypotheses)))))
+  (assoc flow :norm (flow/with-inputs (vec (map (comp unnorm-posterior-key :id) the-hypotheses))
+                      (fn [r] (apply + (map #((-> % :id unnorm-posterior-key) r) hypotheses))))))
 
 (defn add-hypotheses [flow hypotheses]
   (reduce add-hypothesis (normalise-flow flow hypotheses) hypotheses))
 
 ;; ----------------------------------------------
+
+(defn construct-inference [hypotheses]
+  (add-hypotheses (flow/flow) hypotheses))
+
+(defn- inf-keys [id]
+  [(likelihood-key id)
+   (unnorm-posterior-key id)
+   (posterior-key id)])
+
+(defn infer [hypotheses data]
+  (let [inf-graph (construct-inference hypotheses)
+        inf       (flow/run inf-graph {:data data})]
+    (reduce
+     (fn [v {id :id :as h}]
+       (conj v
+             (merge h (select-keys inf (inf-keys id)))))
+     []
+     hypotheses)))
+
+(comment
+  (use 'clojure.pprint)
+
 ;; These likelihoods are whacked !
 (def the-hypotheses
   [{:id :h1, :likelihood-fn inc :prior 2/3}
@@ -72,13 +81,7 @@
    {:id :h3, :likelihood-fn dec :prior 1/3}
    {:id :h4, :likelihood-fn dec :prior 1/3}])
 
-(flow/run
- (add-hypotheses (flow/flow) the-hypotheses)
- {:data 3})
+  (pprint (infer the-hypotheses 3))
 
-(def inference1
-  (add-hypotheses (flow/flow) the-hypotheses))
-
-(flow/write-dotfile inference1 "flow.dot")
-
-(comment dot -Tpng -o flow.png flow.dot)
+  (flow/write-dotfile (construct-inference the-hypotheses) "flow.dot")
+  (comment dot -Tpng -o flow.png flow.dot))
