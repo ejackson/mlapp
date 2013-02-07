075cce834af6c1888b8587fbc81dfb0df754aa3d
Edmund Jackson <edmundsjackson@gmail.com>
2013-02-07 10:20:55 +0000

    More prismatic love.

--- src/mlapp/naive_text.clj
+++ src/mlapp/naive_text.clj
@@ -1,44 +1,46 @@
 (ns mlapp.naive-text
   "The text classification using Naive Bayes and beta-binomial model"
   (:use [plumbing.core])
-  (:require [fs.core :as fs]
-            [stemmers.core :as stem]
-            [clojure.zip :as zip]
+  (:require [fs.core        :as fs]
+            [stemmers.core  :as stem]
+            [clojure.zip    :as zip]
             [plumbing.graph :as graph]))
 
 ;; -----------------------------------------------------------------------------
 ;; Read the files
-(def data-files
+(def data
   {:class-0 {:train {:dir "./resources/data/20news/20news-bydate-train/comp.os.ms-windows.misc/"}
              :test  {:dir "./resources/data/20news/20news-bydate-test/comp.os.ms-windows.misc/"}}
    :class-1 {:train {:dir "./resources/data/20news/20news-bydate-train/comp.windows.x/"}
              :test  {:dir "./resources/data/20news/20news-bydate-test/comp.windows.x/"}}})
 
-(defn- count-map [xs]
-  "Count the number of occurences of each element of seq, return as a map
-   from element to count."
-  (reduce (fn [m x]
-            (update-in m [(keyword x)] (fnil inc 0)))
-          {}
-          xs))
-
-(defn stem-count
-  "Return a hashmap stem -> num-docs-appearing-in of all the stems that appear in a train/test set"
+(defn stem-seq
+  "Return a seq of stems for each doc is dir"
   [dir files]
   (->> files
        (map (partial str dir))           ;; Their full filename
        (map slurp)                       ;; Read em
        (mapcat stem/stems)               ;; Turn each to stems and concat
        (take 1e6)                        ;; Prevent overflow
-       count-map))                       ;; Count each stem occurrance
+       ))
 
-
-(def stats-graph
+(def set-stats
   (graph/eager-compile
-   {:files      (fnk [dir]
+   {:files       (fnk [dir]
                      (fs/list-dir dir))
-    :stem-count (fnk [dir files]
-                     (stem-count dir files))}))
+    :stems       (fnk [dir files]
+                     (stem-seq dir files))
+    :stem-counts (fnk [stems]
+                     (frequencies-fast stems))
+    :num-stems   (fnk [stem-counts]
+                      (reduce + (vals stem-counts)))
+    :stem-freqs  (fnk [stem-counts num-stems]
+                      (map-vals #(/ % num-stems) stem-counts))
+    :num-files   (fnk [files]
+                     (count files))}))
 
-(def g
-  (stats-graph (-> data-files :class-0 :train)))
+(comment
+  (def data2
+    (-> data
+        (update-in [:class-0 :train] set-stats)
+        (update-in [:class-1 :train] set-stats))))
